b6356a6902872622c9af9b9d508e5385532a1bc9
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-02-24 12:08:56 -0800

    add displaying client lists logic and input form

--- app/app.php
+++ app/app.php
@@ -17,21 +17,35 @@
        array('twig.path' => __DIR__.'/../views')
     );
 
+    //RENDER INDEX PAGE
     $app['debug'] = true;
     $app->get("/", function() use ($app){
         return $app['twig']->render('index.html.twig', array("stylists" => Stylist::getAll()));
     });
 
-    $app->post("/add-stylist", function() use ($app) {
+    //RENDER INDEX PAGE (Stylist added)
+    $app->post("/", function() use ($app) {
         $new_stylist = new Stylist(filter_var($_POST['stylist_name'], FILTER_SANITIZE_MAGIC_QUOTES));
         $new_stylist->save();
         return $app['twig']->render('index.html.twig', array("stylists" => Stylist::getAll()));
     });
 
 
+
+    //RENDER STYLIST PAGE
     $app->get("/stylist/{stylist}-{id}", function($stylist, $id) use ($app) {
         $stylist = Stylist::find($id);
         return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
     });
+
+    //RENDER STYLIST PAGE (Client added)
+    $app->post("/stylist/{stylist}-{id}", function($stylist, $id) use ($app) {
+        $stylist = Stylist::find($id);
+        $new_client = new Client(filter_var($_POST['client_name'], FILTER_SANITIZE_MAGIC_QUOTES), $stylist->getStylistId());
+
+        $new_client->save();
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    });
+
 
     return $app;
