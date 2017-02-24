a2f0cc5b04e43ea023aa8de5a37b710410476571
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-02-24 10:08:54 -0800

    add paths to index.html.twig

---
+++ app/app.php
@@ -0,0 +1,27 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Client.php";
+    require_once __DIR__."/../src/Stylist.php";
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app['debug'] = true;
+
+    $app = new Silex\Application();
+    $app->register(
+       new Silex\Provider\TwigServiceProvider(),
+       array('twig.path' => __DIR__.'/../views')
+    );
+
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
